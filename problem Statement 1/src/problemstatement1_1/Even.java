cd240d41a698090914cff430c54239b869e03d13
pasulanikhil-Git <pasulanikhil10@gmail.com>
2022-04-28 15:18:37 +0530

    First commit

---
+++ problem Statement 1/src/problemstatement1_1/Even.java
@@ -0,0 +1,42 @@
+package problemstatement1_1;
+import java.util.*;
+public class Even {
+
+	public static void main(String[] args) {
+
+
+
+		 
+		       int n=0,i=0;
+
+		         
+
+		       Scanner Sc = new Scanner(System.in);
+
+		         
+
+		       System.out.print("Enter value n : ");
+
+		       n = Sc.nextInt();
+
+		         
+
+		       for(i=1; i<n; i++)
+
+		       {
+
+		           if(i%2==0)
+
+		               System.out.print(i+" ");
+
+		       }    
+
+		       System.out.println();
+
+		         
+
+		   }
+
+		
+
+	}
